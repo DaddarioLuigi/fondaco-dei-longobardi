dd56908865041e7f4a1188f778a1b405b7848603
DaddarioLuigi <44373889+DaddarioLuigi@users.noreply.github.com>
2025-01-23 16:38:52 +0100

    update route.js

--- app/api/images/route.js
+++ app/api/images/route.js
@@ -1,24 +1,25 @@
 // app/api/images/route.js
 
-import fs from 'fs'
-import path from 'path'
-import { NextResponse } from 'next/server'
+import fs from 'fs';
+import path from 'path';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  // Percorso assoluto alla cartella `public/images`
-  const imagesDir = path.join(process.cwd(), 'public', 'gallery_images')
-  
-  // Leggi tutti i file nella cartella ` images`
-  const files = fs.readdirSync(imagesDir)
+  // Percorso assoluto alla cartella `public/gallery_images`
+  const imagesDir = path.join(process.cwd(), 'public', 'gallery_images');
 
-  // Filtra .jpg/.jpeg e crea l'array di immagini
+  // Leggi tutti i file nella cartella `gallery_images`
+  const files = fs.readdirSync(imagesDir);
+
+  // Filtra .jpg/.jpeg e crea l'array di immagini ordinato alfabeticamente
   const images = files
     .filter((file) => file.endsWith('.jpg') || file.endsWith('.jpeg'))
+    .sort((a, b) => a.localeCompare(b)) // Ordina alfabeticamente
     .map((file) => ({
       src: `/gallery_images/${file}`,
       alt: file.replace(/\.[^/.]+$/, '').replace(/_/g, ' '),
-    })) 
+    }));
 
   // Restituisce il JSON con NextResponse
-  return NextResponse.json(images)
-} 
+  return NextResponse.json(images);
+}
